1c0e0f4f0857301ae86a0e49c6acc29001afb4b0
JhonatanLop <jhooliveira.lopes@gmail.com>
2023-04-25 17:12:50 -0300

    criando nova tela para veículos

---
+++ telaTeste/org.openjfx_API2Semestre_jar_1.0-SNAPSHOT/src/main/java/org/openjfx/Atividade/VeiculosController.java
@@ -0,0 +1,42 @@
+package org.openjfx.Atividade;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class VeiculosController {
+    @FXML
+    private TextField ano;
+
+    @FXML
+    private TextField capacidade;
+
+    @FXML
+    private Button clean;
+
+    @FXML
+    private Button enviarCadastro;
+
+    @FXML
+    private TextField marca;
+
+    @FXML
+    private TextField peso;
+
+    @FXML
+    void cleanField(ActionEvent event) {
+
+    }
+
+    @FXML
+    void goBack(ActionEvent event) {
+
+    }
+
+    @FXML
+    void sendCad(ActionEvent event) {
+
+    }
+
+}

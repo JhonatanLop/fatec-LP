98724a4d5f673e3dd25b44dceda36b226270e3de
JhonatanLop <jhooliveira.lopes@gmail.com>
2023-06-03 00:05:29 -0300

    arrumando inicialização

--- travel/src/main/java/org/openjfx/travel/App.java
+++ travel/src/main/java/org/openjfx/travel/App.java
@@ -31,8 +31,8 @@ public class App extends Application {
     }
 
     public static void main(String[] args) {
-        // launch();
-        SQLConnection.connect();
+        launch();
+        // SQLConnection.connect();
     }
 
 }

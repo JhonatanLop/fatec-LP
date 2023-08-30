c840ff2d86cef5bd367e04ece60e747038549941
JhonatanLop <jhooliveira.lopes@gmail.com>
2023-08-29 21:52:12 -0300

    complex class fix divisor method

--- LP2/exec/src/main/java/exec/example/exercicio4/Complex.java
+++ LP2/exec/src/main/java/exec/example/exercicio4/Complex.java
@@ -25,11 +25,11 @@ public class Complex {
         this.parteImaine = parteImaine;
     }
 
-    public double getMódulo() {
+    public double getModulo() {
         return Math.sqrt(parteReal * parteReal + parteImaine * parteImaine);
     }
 
-    public double getÂngulo() {
+    public double getAngulo() {
         return Math.atan2(parteImaine, parteReal);
     }
 
@@ -38,16 +38,15 @@ public class Complex {
     }
 
     public Complex somar(Complex num) {
-        return new Complex(parteReal + num.parteReal, parteImaine + num.parteImaine);
+        return new Complex(this.parteReal + num.parteReal, this.parteImaine + num.parteImaine);
     }
 
     public Complex subtrair(Complex num) {
-        return new Complex(parteReal - num.parteReal, parteImaine - num.parteImaine);
+        return new Complex(this.parteReal - num.parteReal, this.parteImaine - num.parteImaine);
     }
 
     public Complex multiplicar(Complex num) {
-        return new Complex((parteReal * num.parteReal) - (parteImaine * num.parteImaine),
-                (parteReal * num.parteImaine) + (parteImaine * num.parteReal));
+        return new Complex((this.parteReal * this.parteImaine) * (num.parteImaine * num.parteImaine));
     }
 
     // essa parte foi braba hein
@@ -55,13 +54,7 @@ public class Complex {
     // a + bi / c + di = (a * c - b * d) / (c² + d²) + (b * c + a * d) / (c² + d²) i
     // onde a e b são as partes reais de a + bi, c e d são as partes reais de c + di, e i é a unidade imaginária.
 
-    public Complex dividir(Complex outroNumero) {
-        return new Complex( (parteReal * outroNumero.parteReal - parteImaine * outroNumero.parteImaine) / (outroNumero.parteReal * outroNumero.parteReal + outroNumero.parteImaine * outroNumero.parteImaine),
-                            (parteImaine * outroNumero.parteReal + parteReal * outroNumero.parteImaine) / (outroNumero.parteReal * outroNumero.parteReal + outroNumero.parteImaine * outroNumero.parteImaine));
-    }
-
-    @Override
-    public String toString() {
-        return String.format("(%.2f,%.2f)", parteReal, parteImaine);
+    public Complex dividir(Complex num) {
+        return new Complex( ((this.parteReal + this.parteImaine) / (num.parteReal + num.parteImaine)) * ((num.parteImaine / num.parteImaine) / (num.parteImaine * num.parteImaine )));
     }
 }

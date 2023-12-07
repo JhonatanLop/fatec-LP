d1fdce75a80d95d80dc3099eda167cd5cad26990
JhonatanLop <jhooliveira.lopes@gmail.com>
2023-12-06 22:07:13 -0300

    retirando caracteres especiais

--- semestre_3/exercicio_final/areachecker/src/main/java/math/area/App.java
+++ semestre_3/exercicio_final/areachecker/src/main/java/math/area/App.java
@@ -12,9 +12,9 @@ public class App
         // dados de exemplo
         Calculable quadrado = ShapeFactory.getShape(ShapeType.SQUARE, new Double[]{5.0});
         Calculable retangulo = ShapeFactory.getShape(ShapeType.RECTANGLE, new Double[]{8.0, 3.0});
-        Calculable círculo = ShapeFactory.getShape(ShapeType.CIRCLE, new Double[]{5.0});
+        Calculable circulo = ShapeFactory.getShape(ShapeType.CIRCLE, new Double[]{5.0});
         Double[] sides = new Double[]{3.0, 4.0, 5.0};
-        Calculable triângulo = ShapeFactory.getShape(ShapeType.TRIANGLE, sides);
+        Calculable triangulo = ShapeFactory.getShape(ShapeType.TRIANGLE, sides);
         Calculable paralelograma = ShapeFactory.getShape(ShapeType.PARALLELOGRAM, new Double[]{5.0, 3.0});
         Calculable trapezio = ShapeFactory.getShape(ShapeType.TRAPEZE, new Double[]{5.0, 3.0, 4.0});
         Calculable hexagono = ShapeFactory.getShape(ShapeType.HEXAGON, new Double[]{5.0});
@@ -25,8 +25,8 @@ public class App
         // saída de dados
         System.out.println("Área do quadrado: " + quadrado.calcularArea());
         System.out.println("Área do retângulo: " + retangulo.calcularArea());
-        System.out.println("Área do círculo: " + círculo.calcularArea());
-        System.out.println("Área do triângulo: " + triângulo.calcularArea());
+        System.out.println("Área do circulo: " + circulo.calcularArea());
+        System.out.println("Área do triangulo: " + triangulo.calcularArea());
         System.out.println("Área do paralelograma: " + paralelograma.calcularArea());
         System.out.println("Área do trapézio: " + trapezio.calcularArea());
         System.out.println("Área do hexágono: " + hexagono.calcularArea());
